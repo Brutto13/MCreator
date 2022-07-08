d798e942bf6332c42a33c96a3722b7b9ab9b21a7
Artkii <askms@192.168.0.164>
2022-07-08 17:14:23 +0200

    Nowy blok "Tasdijfha"

--- src/main/java/net/mcreator/arduinomod/init/ArduinoModModBlocks.java
+++ src/main/java/net/mcreator/arduinomod/init/ArduinoModModBlocks.java
@@ -11,6 +11,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraft.world.level.block.Block;
 
 import net.mcreator.arduinomod.block.WorkStationBlock;
+import net.mcreator.arduinomod.block.TasdjfhaBlock;
 import net.mcreator.arduinomod.block.PCBcreatorBlock;
 import net.mcreator.arduinomod.ArduinoModMod;
 
@@ -18,4 +19,5 @@ public class ArduinoModModBlocks {
 	public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, ArduinoModMod.MODID);
 	public static final RegistryObject<Block> WORK_STATION = REGISTRY.register("work_station", () -> new WorkStationBlock());
 	public static final RegistryObject<Block> PC_BCREATOR = REGISTRY.register("pc_bcreator", () -> new PCBcreatorBlock());
+	public static final RegistryObject<Block> TASDJFHA = REGISTRY.register("tasdjfha", () -> new TasdjfhaBlock());
 }
